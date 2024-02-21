d779cac053c749f3f6bbb75d642b3dc3bf8384e8
TechTastic <74630543+TechTastic@users.noreply.github.com>
2024-02-20 23:44:45 -0600

    Added Github Packages publishing Added Missing Lang for Odor of Purity Progress on WitchesOvenBE

--- common/src/main/kotlin/io/github/techtastic/witcheryrewoven/block/entity/WitchesOvenBE.kt
+++ common/src/main/kotlin/io/github/techtastic/witcheryrewoven/block/entity/WitchesOvenBE.kt
@@ -2,8 +2,16 @@ package io.github.techtastic.witcheryrewoven.block.entity
 
 import io.github.techtastic.witcheryrewoven.block.WRBlockEntities.WITCHES_OVEN
 import net.minecraft.core.BlockPos
+import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
 
-class WitchesOvenBE(blockPos: BlockPos, blockState: BlockState) : BlockEntity(WITCHES_OVEN.get(), blockPos, blockState) {
+class WitchesOvenBE(pos: BlockPos, state: BlockState) : BlockEntity(WITCHES_OVEN.get(), pos, state) {
+    val inventory = listOf(
+            ItemStack.EMPTY,
+            ItemStack.EMPTY,
+            ItemStack.EMPTY,
+            ItemStack.EMPTY,
+            ItemStack.EMPTY
+    )
 }
